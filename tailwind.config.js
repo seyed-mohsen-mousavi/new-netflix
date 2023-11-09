e14da4600d0ad599ca3697380aee0ffbc8cff246
S.mohsen <1mr.rooter3@gmail.com>
2023-11-09 20:23:04 +0330

    up to data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      redi: "#D81F26",
+      white: "#F5F6FF",
+      garyi: "#717489",
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
-}
-
+};
