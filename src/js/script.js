3f3de0eb9f2c6e2280b5e47e189ca4e2790c84b3
S.mohsen <1mr.rooter3@gmail.com>
2023-11-10 23:32:56 +0330

    finish slider

--- src/js/script.js
+++ src/js/script.js
@@ -5,10 +5,18 @@ const imgList = [
   {
     src: "../src/img/lusifer.jpg",
   },
+  {
+    src: "../src/img/mony-honey.jpg"
+  }
 ];
 const slider = document.querySelectorAll(".swiper-slide");
-slider[0].style.backgroundImage = 'url(' + imgList[0].src + ')'
-slider[1].style.backgroundImage = 'url(' + imgList[1].src + ')'
+const sliderimg = document.querySelectorAll("#img-slider");
+
+slider[0].style.backgroundImage = "url(" + imgList[0].src + ")";
+slider[1].style.backgroundImage = "url(" + imgList[1].src + ")";
+slider[2].style.backgroundImage = "url(" + imgList[2].src + ")";
+
+// config Swiper
 const swiper = new Swiper(".img", {
   pagination: {
     el: ".img .swiper-pagination",
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,21 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-      redi: "#D81F26",
-      white: "#F5F6FF",
-      garyi: "#717489",
+    extend: {
+      colors: {
+        redi: "#D81F26",
+        white: "#F5F6FF",
+        garyi: "#717489",
+      },
     },
     container: {
       center: true,
     },
     fontFamily: {
-      'inter': ['inter','sans-self'],
+      inter: ["inter", "sans-self"],
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 };
